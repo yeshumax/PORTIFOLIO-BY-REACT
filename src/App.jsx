578ae1e65a9abcf42745c2dedf8fbe3769db2fb2
Yeshwond Lisanwork <github.com/yeshumax>
2026-01-31 12:22:44 +0300

    Updated weather app with attractive UI, navbar, and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import "./index.css";
+import Weather from "./components/Weather";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Skills from "./pages/Skills";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./App.css";
-
-function App() {
+import Navbar from "./components/Navbar";
+const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Navbar />
+      <Weather />
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
